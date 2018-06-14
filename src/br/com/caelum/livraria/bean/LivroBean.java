0b0a3475cf0aaa7824ed6d9299965de411a0547b
teste <teste@teste-PC>
2018-06-14 00:47:19 -0300

    - atualizando projeto para postgres. - exibindo tabela de livros.

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -32,6 +32,10 @@ public class LivroBean {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 	
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+	
 	public List<Autor> getAutoresDoLivro() {
 		return this.livro.getAutores();
 	}
