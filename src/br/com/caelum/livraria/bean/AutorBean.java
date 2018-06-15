48b870a1cc27fe1c634b90d599cb1edc3642a920
teste <teste@teste-PC>
2018-06-14 23:32:39 -0300

    alterar e remover autores.

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,11 +1,15 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -14,13 +18,30 @@ public class AutorBean {
 		return autor;
 	}
 
-	public String gravar() {
+	public void gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
-
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		
+		if(this.autor.getId() != null) {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		}
 		
 		this.autor = new Autor();
 		
-		return "livro?faces-redirect=true";
+//		return "livro?faces-redirect=true";
 	}
+	
+	public List<Autor> getListaAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void carregar(Autor autor) {
+		this.autor = autor;
+	}
+	
+	public void remover(Autor autor) {
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
 }
